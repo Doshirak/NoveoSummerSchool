1b054a3cebfe97220861e388b951e736515f30f0
Maxim Ten <maximten94@gmail.com>
2014-07-15 17:20:11 +0700

    Modify bonusTask

--- PHP/ClassWork/day1/bonusTask.php
+++ PHP/ClassWork/day1/bonusTask.php
@@ -3,20 +3,32 @@
 $month = date('F');
 $daysNumber = date('t');
 $currDay = 1;
+$currDayInWeek = date('w',strtotime("Sunday + $currDay Days")) + 1;
 
+// Month
 echo($month."\n");
+
+// Week days
 for ($i = 0;$i < 7;++$i)
 {
-	echo(date('D',strtotime("Sunday + $i Days")).' ');
+	echo(date('D',strtotime("Monday + $i Days")).' ');
 }
 echo("\n");
+
+// Numbers of days
+for ($i = 0;$i < $currDayInWeek - 1;++$i)
+{
+	echo("    ");
+}
 while ($currDay < $daysNumber)
 {
 	printf('%3d ',$currDay);
-	if (($currDay % 7) == 0){
+	if (($currDayInWeek % 7) == 0){
+		$currDayInWeek = 0;
 		echo("\n");
 	}
 	++$currDay;
+	++$currDayInWeek;
 }
 echo("\n");
 
