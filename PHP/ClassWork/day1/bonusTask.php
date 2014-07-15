3118b3e953a403ab1ae6b351be20b6d52191f798
Maxim Ten <maximten94@gmail.com>
2014-07-15 16:53:32 +0700

    Modify bonus task

--- PHP/ClassWork/day1/bonusTask.php
+++ PHP/ClassWork/day1/bonusTask.php
@@ -5,9 +5,14 @@ $daysNumber = date('t');
 $currDay = 1;
 
 echo($month."\n");
+for ($i = 0;$i < 7;++$i)
+{
+	echo(date('D',strtotime("Sunday + $i Days")).' ');
+}
+echo("\n");
 while ($currDay < $daysNumber)
 {
-	printf('%2d ',$currDay);
+	printf('%3d ',$currDay);
 	if (($currDay % 7) == 0){
 		echo("\n");
 	}
