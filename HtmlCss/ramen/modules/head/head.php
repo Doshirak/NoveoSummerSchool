3bd12279273bbd4b61e24342ed8a00ea685f937f
Maxim Ten <maximten94@gmail.com>
2014-07-21 19:32:42 +0700

    Modify ramen.org
    Add menu and search bar

--- HtmlCss/ramen/modules/head/head.php
+++ HtmlCss/ramen/modules/head/head.php
@@ -1,8 +1,59 @@
 <div id="header" class="container colorA">
 	<div class="row element">
 		<div class="logo"><img src="images/logo.png" alt="Logo of ramen.org" width="80" height="80"/></div>
-		<div class="imageColor search">Search bar</div>
+		<div class="searchFrame">
+			<form>
+                                <input class="search header" type="text" placeholder="Search..." required>
+                                <input class="button header" type="button" value="Search">
+			</form>			 
+		</div>
 	</div>
-	<div class="menu element textColor">Menu</div>
+	<div class="menu header">
+		<ul id="menu">
+		    <li>
+		        <a href="#">Menu 1</a>
+		    </li>
+		    <li><a href="#">Menu 2</a>
+
+		        <ul class="sub-menu">
+		            <li>
+		                <a href="#">Sub Menu 1</a>
+		            </li>
+		            <li>
+		                <a href="#">Sub Menu 2</a>
+		            </li>
+		            <li>
+		                <a href="#">Sub Menu 3</a>
+		            </li>
+		            <li>
+		                <a href="#">Sub Menu 4</a>
+		            </li>
+		        </ul>
+		    </li>
+		    <li><a href="#">Menu 3</a>
+
+		    </li>
+		    <li><a href="#">Menu 4</a>
+
+		        <ul class="sub-menu">
+		            <li>
+		                <a href="#">Sub Menu 1</a>
+		            </li>
+		            <li>
+		                <a href="#">Sub Menu 2</a>
+		            </li>
+		            <li>
+		                <a href="#">Sub Menu 3</a>
+		            </li>
+		            <li>
+		                <a href="#">Sub Menu 4</a>
+		            </li>
+		        </ul>
+		    </li>
+		    <li>
+		        <a href="#">Menu 5</a>
+		    </li>
+		</ul>
+	</div>
 </div>
 <div class="line"></div>
